eb83d0a8f0b1ce09ca8bbfb01f56bff8a335dc5e
ozelifaydin <ozelfiaydin@gmail.com>
2020-02-13 17:05:43 -0600

    sarkilar seni soyler

--- src/org/launchcode/ThirdLaunchcode.java
+++ src/org/launchcode/ThirdLaunchcode.java
@@ -5,5 +5,6 @@ public class ThirdLaunchcode {
         System.out.println("Elif Aydin");
         System.out.println("zeynepcik");
         System.out.println("Hi");
+        System.out.println("huysuz ve tatli kadin");
     }
 }
