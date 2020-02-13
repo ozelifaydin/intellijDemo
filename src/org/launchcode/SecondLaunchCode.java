47cb4fffc965942585bdaf45b9fafd11eeda025a
ozelifaydin <ozelfiaydin@gmail.com>
2020-02-13 16:48:29 -0600

    Third commit message

--- src/org/launchcode/SecondLaunchCode.java
+++ src/org/launchcode/SecondLaunchCode.java
@@ -3,5 +3,6 @@ package org.launchcode;
 public class SecondLaunchCode {
     public static void main(String[] args) {
         System.out.println("Merhaba Turkiye");
+        System.out.println("hey george versene borc");
     }
 }
